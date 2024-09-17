186cf4186306732336be8489131780e72177cb3b
kurves <kavssimply@gmail.com>
2024-09-17 11:50:21 +0000

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+      if (lines.length <= 1) {
+        throw new Error('Cannot load the database');
+      }
+
+      const students = lines.slice(1);  // Skip the header line
+      console.log(`Number of students: ${students.length}`);
+
+      const fields = {};
+      students.forEach((line) => {
+        const [firstName, , , field] = line.split(',');
+        if (fields[field]) {
+          fields[field].push(firstName);
+        } else {
+          fields[field] = [firstName];
+        }
+      });
+
+      for (const [field, names] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+    })
+    .catch((error) => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
