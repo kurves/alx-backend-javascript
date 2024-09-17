65be20e4db0cc78e6dd07635a2338066b6f9848a
kurves <kavssimply@gmail.com>
2024-09-17 13:01:50 +0000

    script to server up app

---
+++ 0x05-Node_JS_basic/full_server/routes/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use('/', indexRouter);
+
+const PORT = 1245;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
