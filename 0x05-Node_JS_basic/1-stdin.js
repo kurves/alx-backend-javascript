d48c191ff56851f9173ba06fec7c976c79e83302
kurves <kavssimply@gmail.com>
2024-10-18 11:05:51 +0000

    using proces stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,16 @@
-// Display the initial message
-console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-
-    process.stdin.end();
-});
-
 process.on('exit', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
+});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
