33eb6054466ff70b89223935ade4425e557b0a1a
kurves <kavssimply@gmail.com>
2024-10-18 11:20:32 +0000

    process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,11 @@
-// Import required modules
+// Import required moduleis
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
   if (chunk) {
-    // Trim the input to remove any trailing newlines or spaces
-    const name = chunk.trim();
-    process.stdout.write(`Your name is: ${name}\n`);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
